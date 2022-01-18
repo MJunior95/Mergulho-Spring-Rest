50569d548787efa0ae21606cb5703c1be3d58363
Michel Junior <jrcassunde95@gmail.com>
2022-01-18 12:12:24 -0300

    alterando datas para OffsetDateTime

--- src/main/java/com/algaworks/algalog/domain/service/SolicitacaoEntregaService.java
+++ src/main/java/com/algaworks/algalog/domain/service/SolicitacaoEntregaService.java
@@ -1,16 +1,14 @@
 package com.algaworks.algalog.domain.service;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
-import com.algaworks.algalog.domain.exception.NegocioException;
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.model.Entrega;
 import com.algaworks.algalog.domain.model.StatusEntrega;
-import com.algaworks.algalog.domain.repository.ClienteRepository;
 import com.algaworks.algalog.domain.repository.EntregaRepository;
 
 import lombok.AllArgsConstructor;
@@ -28,7 +26,7 @@ public class SolicitacaoEntregaService {
 				
 		entrega.setCliente(cliente);
 		entrega.setStatus(StatusEntrega.PENDENTE);
-		entrega.setDataPedido(LocalDateTime.now());
+		entrega.setDataPedido(OffsetDateTime.now());
 		
 		return entregaRepository.save(entrega);
 	}
